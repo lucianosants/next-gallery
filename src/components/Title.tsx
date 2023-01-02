bc372032f4232322c083cdd194b77dff738cbbb1
lucianosants <lucianola18@gmail.com>
2023-01-02 14:36:10 -0300

    feat: initial setup

---
+++ src/components/Title.tsx
@@ -0,0 +1,7 @@
+export default function Title() {
+    return (
+        <div className="text-2xl text-center text-fuchsia-700">
+            Next Gallery
+        </div>
+    );
+}
---
+++ src/screens/HomePage/index.tsx
@@ -0,0 +1,5 @@
+import Title from '@src/components/Title';
+
+export default function HomePage() {
+    return <Title />;
+}
