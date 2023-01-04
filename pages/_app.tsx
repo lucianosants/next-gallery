fdb00a06b9cc99e3e7d7a771ca833a9a093bed90
lucianosants <lucianola18@gmail.com>
2023-01-04 11:40:23 -0300

    style: render footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../src/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '@src/components/Header';
+import Footer from '@src/components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Header />
             <Component {...pageProps} />
+            <Footer />
         </>
     );
 }
