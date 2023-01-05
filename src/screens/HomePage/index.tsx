43c41c9c3469cb8509afdf47eb6417cb49da345c
lucianosants <lucianola18@gmail.com>
2023-01-05 15:40:37 -0300

    style: render Container and several images

--- src/screens/HomePage/index.tsx
+++ src/screens/HomePage/index.tsx
@@ -1,5 +1,18 @@
-import Title from '@src/components/Title';
+import Container from '@src/components/Container';
+import Image from '@src/components/Image';
 
 export default function HomePage() {
-    return <Title />;
+    return (
+        <Container>
+            <Image src="https://source.unsplash.com/random/1" alt="" link="#" />
+            <Image src="https://source.unsplash.com/random/2" alt="" link="#" />
+            <Image src="https://source.unsplash.com/random/3" alt="" link="#" />
+            <Image src="https://source.unsplash.com/random/4" alt="" link="#" />
+            <Image src="https://source.unsplash.com/random/5" alt="" link="#" />
+            <Image src="https://source.unsplash.com/random/6" alt="" link="#" />
+            <Image src="https://source.unsplash.com/random/7" alt="" link="#" />
+            <Image src="https://source.unsplash.com/random/8" alt="" link="#" />
+            <Image src="https://source.unsplash.com/random/9" alt="" link="#" />
+        </Container>
+    );
 }
