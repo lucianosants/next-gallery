3eb40bf51db8a82eea83082422e893b080fcd877
lucianosants <lucianola18@gmail.com>
2023-01-07 10:48:25 -0300

    feat: add types for photos

---
+++ src/types/photos.ts
@@ -0,0 +1,36 @@
+export type PhotoId = string;
+
+export type PhotoImage = {
+    files: [
+        {
+            file: {
+                url: string;
+            };
+        }
+    ];
+};
+
+export type PhotoTag = {
+    select: {
+        name: string;
+    };
+};
+
+export type PhotoTitle = {
+    title: [
+        {
+            plain_text: string;
+        }
+    ];
+};
+
+export type PhotoProperties = {
+    image: PhotoImage;
+    tag: PhotoTag;
+    title: PhotoTitle;
+};
+
+export type PhotosData = {
+    id: PhotoId;
+    properties: PhotoProperties;
+};
