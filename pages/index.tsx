8ce255c5f0b6cb337741fdaf4bdba6d9314bcf28
lucianosants <lucianola18@gmail.com>
2023-01-08 07:23:49 -0300

    style: try back to ssr without any lib

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,34 @@
 import Head from 'next/head';
-import useSWR from 'swr';
+import { GetServerSideProps } from 'next';
+
+import { getAllPhotos } from '@src/data/photos/get-all-photos';
+import { PhotosData } from '@src/types/photos';
 
 import HomePage from '@src/screens/HomePage';
-import { fetcher } from '@src/utils/fetcher';
 
-const Home = () => {
-    const { data } = useSWR('/api/photos', fetcher);
+interface HomeProps {
+    data: PhotosData[];
+}
 
+const Home = ({ data }: HomeProps) => {
     return (
         <>
             <Head>
                 <title>Next Gallery</title>
             </Head>
+
             <HomePage photos={data} />
         </>
     );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const url = 'http://localhost:3000/api/photos';
+    const data = await getAllPhotos(url);
+
+    return {
+        props: { data },
+    };
 };
 
 export default Home;
