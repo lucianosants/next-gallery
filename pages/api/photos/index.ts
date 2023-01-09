ba7db712c4baee89c4818d3658a19e5b29a24397
lucianosants <lucianola18@gmail.com>
2023-01-09 09:30:49 -0300

    refactor: call notion client utils

--- pages/api/photos/index.ts
+++ pages/api/photos/index.ts
@@ -1,18 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { Client } from '@notionhq/client';
-
-const notion = new Client({
-    auth: process.env.NOTION_API_KEY,
-});
+import { DATABASE_ID } from '@src/config/constants';
+import { notion } from '@src/utils/notion-client';
 
 export default async function getPhotos(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     try {
-        const DATABASE_ID = '3ec14f47e91446c5bf1a1b37cabe7b5b';
-
         const response = await notion.databases.query({
             database_id: DATABASE_ID,
         });
