80f1ff0fe6883832858f79016f44982cb067e6b8
lucianosants <lucianola18@gmail.com>
2023-01-13 11:25:05 -0300

    feat: add screen to photo page

---
+++ src/screens/PhotoPage/index.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router';
+import style from './PhotoPage.module.css';
+
+interface PhotoPageProps {
+    url: string;
+    title: string;
+}
+
+export default function PhotoPage({ url, title }: PhotoPageProps) {
+    const router = useRouter();
+    console.log(router);
+    return (
+        <div className={style.photo}>
+            <button
+                type="button"
+                className={style.photo__button}
+                title="Back"
+                onClick={() => router.back()}>
+                &larr; Back
+            </button>
+
+            <h2 className={style.photo__title}>{title}</h2>
+
+            <div className={style.photo__content}>
+                <img
+                    className={style.photo__image}
+                    src={url}
+                    alt={title}
+                    title={title}
+                />
+            </div>
+        </div>
+    );
+}
